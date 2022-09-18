fa09d0c82133e127f2a381906c1971ee8be2569f
QSL <kbeechly@icloud.com>
2022-09-18 13:28:39 -0700

    Add tests

--- app/battleship.test.js
+++ app/battleship.test.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-undef */
 import battleship from "./battleship";
 
-test("gets name", () => {
+test("shows name", () => {
   expect(battleship.getName()).toBe("Battleship");
+});
+
+test("shows HP", () => {
+  expect(battleship.getHP()).toBe(5);
+});
+
+test("gets hit", () => {
+  expect(battleship.hit(6, 9), battleship.getHP()).toBe(4);
 });
