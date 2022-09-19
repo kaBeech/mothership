b69069ba4766160e61896b31c3e1f158318b5614
QSL <kbeechly@icloud.com>
2022-09-18 21:16:12 -0700

    Rearrange battleship.js

--- app/battleship.js
+++ app/battleship.js
@@ -3,6 +3,13 @@ import Player from "./Player";
 
 const player1Gameboard = Gameboard();
 player1Gameboard.init();
+
+const player2Gameboard = Gameboard();
+player2Gameboard.init();
+
+const player1 = Player("Alice", player1Gameboard, player2Gameboard);
+const player2 = Player("Bob", player2Gameboard, player1Gameboard);
+
 player1Gameboard.addShip("Aircraft Carrier", [
   "20",
   "30",
@@ -16,8 +23,6 @@ player1Gameboard.addShip("Cruiser", ["28", "38", "48", "58"]);
 player1Gameboard.addShip("Gunboat", ["27", "37", "47"]);
 player1Gameboard.addShip("Submarine", ["26", "36"]);
 
-const player2Gameboard = Gameboard();
-player2Gameboard.init();
 player2Gameboard.addShip("Aircraft Carrier", [
   "21",
   "31",
@@ -30,8 +35,5 @@ player2Gameboard.addShip("Battleship", ["22", "32", "42", "52", "62"]);
 player2Gameboard.addShip("Cruiser", ["23", "33", "43", "53"]);
 player2Gameboard.addShip("Gunboat", ["24", "34", "44"]);
 player2Gameboard.addShip("Submarine", ["25", "35"]);
-
-const player1 = Player("Alice", player1Gameboard, player2Gameboard);
-const player2 = Player("Bob", player2Gameboard, player1Gameboard);
 
 export { player1, player2, player1Gameboard, player2Gameboard };
