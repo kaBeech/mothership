4c4e1fa4b2edbe4cc86c13616d8c6f7739230ff6
QSL <kbeechly@icloud.com>
2022-10-01 13:22:30 -0700

    Cleanup

--- app/mothership.js
+++ app/mothership.js
@@ -1,28 +1,5 @@
 /* eslint-disable no-param-reassign */
-import Gameboard from "./Gameboard";
-import Player from "./Player";
 import gameController from "./gameController";
-
-const player1Gameboard = Gameboard();
-player1Gameboard.init();
-
-const player2Gameboard = Gameboard();
-player2Gameboard.init();
-
-const player1 = Player("Alice", player1Gameboard, player2Gameboard, "computer");
-const player2 = Player("Bob", player2Gameboard, player1Gameboard, "human");
-
-player1Gameboard.addShip("Mothership", ["20", "30", "40", "50", "60", "70"]);
-player1Gameboard.addShip("Battleship", ["29", "39", "49", "59", "69"]);
-player1Gameboard.addShip("Cruiser", ["28", "38", "48", "58"]);
-player1Gameboard.addShip("Gunship", ["27", "37", "47"]);
-player1Gameboard.addShip("Starfighter", ["26", "36"]);
-
-player2Gameboard.addShip("Mothership", ["21", "31", "41", "51", "61", "71"]);
-player2Gameboard.addShip("Battleship", ["22", "32", "42", "52", "62"]);
-player2Gameboard.addShip("Cruiser", ["23", "33", "43", "53"]);
-player2Gameboard.addShip("Gunship", ["24", "34", "44"]);
-player2Gameboard.addShip("Starfighter", ["25", "35"]);
 
 const currentPlayerSetter = (state) => ({
   setCurrentPlayer: (player) => {
@@ -106,11 +83,4 @@ const mothership = (() => {
   };
 })();
 
-export {
-  mothership,
-  gameController,
-  player1,
-  player2,
-  player1Gameboard,
-  player2Gameboard,
-};
+export { mothership, gameController };
