e7ed93a2b2016c1c0c44e1d2a056fc1220894ca5
QSL <kbeechly@icloud.com>
2022-10-11 18:26:02 -0700

    Add Start Game Button

--- mothership/src/App.js
+++ mothership/src/App.js
@@ -4,12 +4,13 @@ import GameboardDOM from "./components/GameboardDOM";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="flex column">
         <h1> Hello World! </h1>
         <div className="flex">
           <GameboardDOM player="p1" />
           <GameboardDOM player="p2" />
         </div>
+        <div className="button">Start Game</div>
       </div>
     );
   }
