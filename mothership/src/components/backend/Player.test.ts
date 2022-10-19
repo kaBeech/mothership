36c05e69191f318e363c8bf821549838cb727486
QSL <kbeechly@icloud.com>
2022-10-18 20:51:15 -0700

    Add boilerplate tests for remaining backend components

---
+++ mothership/src/components/backend/Player.test.ts
@@ -0,0 +1,7 @@
+import Player from "./Player";
+
+test("all tests written", () => {
+  expect(Player).toBe(
+    "don't change this until tests have been added for all public methods"
+  );
+});
---
+++ mothership/src/components/backend/tools.test.ts
@@ -0,0 +1,7 @@
+import tools from "./tools";
+
+test("all tests written", () => {
+  expect(true).toBe(
+    "don't change this until tests have been added for all public methods"
+  );
+});
