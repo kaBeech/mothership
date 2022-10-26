9bbc81bed8b064605eae7d08574a07ccc6ecd4d2
QSL <kbeechly@icloud.com>
2022-10-26 01:02:17 -0700

    Adjust test wording and logic

--- mothership/src/components/backend/mothership.test.ts
+++ mothership/src/components/backend/mothership.test.ts
@@ -1,9 +1,5 @@
 import mothership from "./mothership";
 
-test.skip("Game starts with computer move", () => {
-  expect(mothership.startGame()).toBe("Prompted human player");
-});
-
 test("Initial phase returns null", () => {
   expect(mothership.getCurrentPhase()).toBe(null);
 });
@@ -22,35 +18,33 @@ test("Selecting an attack before starting the game throws error", () => {
   );
 });
 
+test("Game starts", () => {
+  expect(mothership.startGame()).toBe("Prompted human player");
+});
+
 test("Attempting to start the game while game is already in progress throws error", () => {
-  mothership.startGame();
   expect(mothership.startGame()).toBe("Error: Game already in progress!");
 });
 
 test("Phase is 'Waiting for human player' after starting the game", () => {
-  mothership.startGame();
   expect(mothership.getCurrentPhase()).toBe("Waiting for human player");
 });
 
 test("Current player is player0 after starting the game", () => {
-  mothership.startGame();
   expect(mothership.getCurrentPlayer().getID()).toBe("player0");
 });
 
 test("Opposing player is player1 after starting the game", () => {
-  mothership.startGame();
   expect(mothership.getOpposingPlayer().getID()).toBe("player1");
 });
 
 test("Selecting an attack on currentPlayer's own board throws error", () => {
-  mothership.startGame();
   expect(mothership.receiveAttackSelection("24p0")).toBe(
     "Error: Select an attack on your opponant's gameboard, not your own!"
   );
 });
 
 test("Selecting a valid attack progresses loop back to next human player turn", () => {
-  mothership.startGame();
   expect(mothership.receiveAttackSelection("24p1")).toBe(
     "Prompted human player"
   );
